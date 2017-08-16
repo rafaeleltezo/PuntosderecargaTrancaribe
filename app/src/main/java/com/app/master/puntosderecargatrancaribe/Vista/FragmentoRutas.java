297b43d0fe89caba5c59f6ae71279185a472c873
Rafael p <barboza.rafael.p@gmail.com>
2017-08-15 21:58:16 -0500

    Terminando sugerencia de rutas principales

--- app/src/main/java/com/app/master/puntosderecargatrancaribe/Vista/FragmentoRutas.java
+++ app/src/main/java/com/app/master/puntosderecargatrancaribe/Vista/FragmentoRutas.java
@@ -38,78 +38,113 @@ public class FragmentoRutas extends Fragment {
 
         vista=inflater.inflate(R.layout.fragment_rutas, container, false);
         contador=0;
-        ArrayList<Bus> busesCastellana=new ArrayList();
+
+        paraderos=datosParadero();
+        ArrayList<Bus> busesBombaGallo=new ArrayList();
         //buses de la castellana
-        busesCastellana.add(new Bus("xt108","Variante"));
-        busesCastellana.add(new Bus("xt1010","Bocagrande"));
-        //busesCastellana.add(new Bus("xt101","Todas las paradas"));
-        paraderoOrigen=new ParaderoBuscador(busesCastellana,"Castellana","Paradero de la castellana","castellana,Exito cartagena,",0,0,0.1);
-        buscadorParaderoDestino("crespo");
+        // busesBombaGallo.add(new Bus("t102","Crespo"));
+        busesBombaGallo.add(new Bus("t101","Portal"));
+        busesBombaGallo.add(new Bus("t103","Bocagrande"));
+        busesBombaGallo.add(new Bus("t100","Expresa"));
+        paraderoOrigen=new ParaderoBuscador(busesBombaGallo,"Bomba del gallo","Paradero frente Bomba del gallo","Bomba del gallo",0,0,1);
+        buscadorParaderoDestino("exito cartagena");
+
             if(paraderoDestino.getPosicion()>paraderoOrigen.getPosicion()){
                 rutaCercana(paraderoOrigen,paraderoDestino);
+
             }
-            else {
+            else if(paraderoDestino.getPosicion()<paraderoOrigen.getPosicion()) {
+
+
+                double origen=paraderoOrigen.getPosicion();
+                paraderoOrigen.setPosicion(-1*origen);
                 convertirPosicionParaderosNegativo();
+                rutaCercana(paraderoOrigen,paraderoDestino);
             }
+
 
         return vista;
     }
 
     public void convertirPosicionParaderosNegativo(){
-        for (int i = 0; i <paraderos.size() ; i++) {
 
+        for (int i = 0; i <paraderos.size() ; i++) {
+            double posicion=paraderos.get(i).getPosicion();
+            paraderos.get(i).setPosicion(-1*posicion);
         }
+
     }
     //datos entrantes
     public ArrayList<ParaderoBuscador> datosParadero(){
         paraderos=new ArrayList();
 
+        ArrayList<Bus> busesBombaGallo=new ArrayList();
+        //buses de la castellana
+       // busesBombaGallo.add(new Bus("t102","Crespo"));
+        busesBombaGallo.add(new Bus("t101","Portal"));
+        busesBombaGallo.add(new Bus("t103","Bocagrande"));
+        busesBombaGallo.add(new Bus("t100","Expresa"));
+        paraderos.add(new ParaderoBuscador(busesBombaGallo,"Bomba del gallo","Paradero frente Bomba del gallo","Bomba del gallo",0,0,1));
+
         ArrayList<Bus> busesAlimentador1Castellana=new ArrayList();
         //buses de la castellana
-        busesAlimentador1Castellana.add(new Bus("xt108","Variante"));
-        busesAlimentador1Castellana.add(new Bus("xt105","Bocagrande"));
+        busesAlimentador1Castellana.add(new Bus("t106","Variante"));
+        busesAlimentador1Castellana.add(new Bus("t10","Bocagrande"));
         //busesCastellana.add(new Bus("xt101","Todas las paradas"));
-        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"bomba el amparo","Paradero frente bomba el amparo","bomba el amparo,cai bomba el amparo,",0,0,0.1));
+        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"bomba el amparo","Paradero frente bomba el amparo","bomba el amparo,cai bomba el amparo,",0,0,1.1));
 
         ArrayList<Bus> busesAlimentador2Castellana=new ArrayList();
         //buses de la castellana
-        busesAlimentador2Castellana.add(new Bus("xt108","Variante"));
-        busesAlimentador2Castellana.add(new Bus("xt105","Bocagrande"));
+        busesAlimentador2Castellana.add(new Bus("t106","Variante"));
+        busesAlimentador2Castellana.add(new Bus("t10","Bocagrande"));
         //busesCastellana.add(new Bus("xt101","Todas las paradas"));
-        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"sao","Paradero frente sao","sao",0,0,0.2));
+        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"sao","Paradero frente sao","sao",0,0,1.2));
 
         ArrayList<Bus> busesAlimentador3Castellana=new ArrayList();
         //buses de la castellana
-        busesAlimentador3Castellana.add(new Bus("xt105","Bocagrande"));
+        busesAlimentador3Castellana.add(new Bus("t106","Variante"));
         //busesCastellana.add(new Bus("xt101","Todas las paradas"));
-        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"sanjose","Paradero frente sanjose","san jose",0,0,0.3));
+        paraderos.add(new ParaderoBuscador(busesAlimentador1Castellana,"sanjose","Paradero frente sanjose","san jose",0,0,1.3));
 
         ArrayList<Bus> busesCastellana=new ArrayList();
         //buses de la castellana
-        busesCastellana.add(new Bus("xt108","Variante"));
-        busesCastellana.add(new Bus("xt101","Bocagrande"));
-        //busesCastellana.add(new Bus("xt101","Todas las paradas"));
-        paraderos.add(new ParaderoBuscador(busesCastellana,"Castellana","Paradero de la castellana","castellana,Exito cartagena,",0,0,1));
+        busesCastellana.add(new Bus("t102","Crespo"));
+        busesCastellana.add(new Bus("t101","Portal"));
+        busesCastellana.add(new Bus("t103","Bocagrande"));
+        busesCastellana.add(new Bus("t106","Variante"));
+        busesCastellana.add(new Bus("t100","Expresa"));
+        paraderos.add(new ParaderoBuscador(busesCastellana,"Castellana","Paradero de la castellana","castellana,Exito cartagena,",0,0,2));
         //buses de cuatro vientos
-        ArrayList<Bus> busesCuatroVientos=new ArrayList();
-        busesCuatroVientos.add(new Bus("xt109","Bocagrande"));
-        busesCuatroVientos.add(new Bus("xt101","Crespo"));
-        paraderos.add(new ParaderoBuscador(busesCuatroVientos,"Cuatro vientos","paradero Cuatro viento","cuatro viento,frente sena,sena",0,9,2));
+        ArrayList<Bus> losejecutivos=new ArrayList();
+        losejecutivos.add(new Bus("t103","Bocagrande"));
+        losejecutivos.add(new Bus("t106","Variante"));
+        losejecutivos.add(new Bus("t101","Portal"));
+        paraderos.add(new ParaderoBuscador(losejecutivos,"Cuatro vientos","paradero Cuatro viento","cuatro viento,frente sena,sena",0,9,3));
         //paradero inventado
-        ArrayList<Bus> busesInventado=new ArrayList();
-        busesInventado.add(new Bus("xt108","Bocagrande"));
-        busesInventado.add(new Bus("xt1011","Crespo"));
-        paraderos.add(new ParaderoBuscador(busesInventado,"inventado","paradero inventado","inventado, viento,",0,9,3));
 
-        ArrayList<Bus> busesInventado2=new ArrayList();
-        busesInventado2.add(new Bus("xt108","Bocagrande"));
-        busesInventado2.add(new Bus("xt101","Crespo"));
-        paraderos.add(new ParaderoBuscador(busesInventado2,"inventado2","paradero inventado2","inventado, viento,",0,9,4));
+        ArrayList<Bus> busesVillaOlimpica=new ArrayList();
+        busesVillaOlimpica.add(new Bus("t101","Portal"));
+        paraderos.add(new ParaderoBuscador(busesVillaOlimpica,"Villa olimpica","paradero Villa olimpica","Estadio jaime moron, villa olimpica,",0,9,4));
+
+        ArrayList<Bus> busesCuatroViento=new ArrayList();
+        busesCuatroViento.add(new Bus("t101","Portal"));
+        busesCuatroViento.add(new Bus("t102","Crespo"));
+        busesCuatroViento.add(new Bus("t103","Bocagrande"));
+        paraderos.add(new ParaderoBuscador(busesCuatroViento,"Cuatro vientos","paradero Cuatro vientos","cuatro vientos,frente sena cuatro viento,",0,9,5));
+
         //buses de crespo
-        ArrayList<Bus> busesCuatroCrespo=new ArrayList();
-        busesCuatroCrespo.add(new Bus("xt1088","Bocagrande"));
-        busesCuatroCrespo.add(new Bus("xt101","Crespo"));
-        paraderos.add(new ParaderoBuscador(busesCuatroCrespo,"crespo","paradero crespo","crespo,prueba",0,9,5));
+        ArrayList<Bus> busesMariaAuxiliadora=new ArrayList();
+        busesMariaAuxiliadora.add(new Bus("t106","Variante"));
+        busesMariaAuxiliadora.add(new Bus("t101","Portal"));
+        paraderos.add(new ParaderoBuscador(busesMariaAuxiliadora,"Maria auxiliadora","paradero Maria auxiliadora","maria auxiliadora,cai maria auxiliadora",0,9,6));
+
+
+        ArrayList<Bus> busesBasurto=new ArrayList();
+        //busesBasurto.add(new Bus("t106","Variante"));
+        busesBasurto.add(new Bus("t102","Crespo"));
+        //busesBasurto.add(new Bus("t103","Bocagrande"));
+        paraderos.add(new ParaderoBuscador(busesBasurto,"Basurto","paradero Basurto","mercado Basurto",0,9,7));
+
         return paraderos;
     }
 
@@ -120,7 +155,7 @@ public class FragmentoRutas extends Fragment {
 
         //buscador de paradero destino con el nombre solicitado por el usuario
 
-        for (ParaderoBuscador paradero:datosParadero()) {
+        for (ParaderoBuscador paradero:paraderos) {
             for (String palabras:descomponerPalabra(paradero.getPalabrasClaves())) {
                 if (palabras.equals(destino)) {
                     //Toast.makeText(getContext(), "Encontrado "+"Nombre del paradero: "+paradero.getNombre(), Toast.LENGTH_SHORT).show();
@@ -155,14 +190,16 @@ public class FragmentoRutas extends Fragment {
         if(obtenerBusesParaAbordar(origen,destino).size()==1){
             buses.add(obtenerBusesParaAbordar(origen,destino).get(0));
             imprimir(buses);
+
             //Toast.makeText(getContext(), "Aborda el " + obtenerBusesParaAbordar(origen,destino).get(0).getNombre(), Toast.LENGTH_SHORT).show();
             return true;
         }else if(obtenerBusesParaAbordar(origen,destino).size()>1){
             ArrayList<Bus>bus=filtroBusMenosParadas(obtenerBusesParaAbordar(origen,destino));
             filtroBusMenosParadas(obtenerBusesParaAbordar(origen,destino));
-
+            Toast.makeText(getContext(), String.valueOf(bus.size()), Toast.LENGTH_SHORT).show();
             if(bus.size()==1) {
                 buses.add(bus.get(0));
+
                 imprimir(buses);
                 //Toast.makeText(getContext(), "Aborda: " + bus.get(0).getNombre(), Toast.LENGTH_SHORT).show();
                 return true;
@@ -179,6 +216,7 @@ public class FragmentoRutas extends Fragment {
        if(obtenerBusesParaAbordar(origen,destino).size()==0){
 
            Toast.makeText(getContext(), "Rutas alternativas", Toast.LENGTH_SHORT).show();
+           //Toast.makeText(getContext(), paraderoDestino.getNombre(), Toast.LENGTH_SHORT).show();
            rutaTransbordo();
        }
 
@@ -196,20 +234,41 @@ public class FragmentoRutas extends Fragment {
 
        // if(paraderoOrigen.getPosicion()>0 && paraderoDestino.getPosicion()>0) {
             contador++;
-            double paraderoPosterior = paraderoDestino.getPosicion() + 1;
-            double paraderoAnterior = paraderoDestino.getPosicion() - contador;
+            int paraderoPosterior = (int)paraderoDestino.getPosicion() + contador;
+            int paraderoAnterior  = (int)paraderoDestino.getPosicion() - contador;
+
+        String cadena=String.valueOf(paraderoDestino.getPosicion());
+
+            if(cadena.indexOf("."+1)!=0){
+                Toast.makeText(getContext(), "es un paradero con dcimal", Toast.LENGTH_SHORT).show();
+            }
 
 
 
-            if(paraderoOrigen.getPosicion()>0 && paraderoDestino.getPosicion()>0){
-                if (rutaCercana(paraderoOrigen, paraderos.get((int)paraderoAnterior - 1))) {
-                    rutaCercana(paraderos.get((int)paraderoAnterior - 1), paraderoDestino);
+        try {
+            Toast.makeText(getContext(), String.valueOf(paraderoPosterior), Toast.LENGTH_SHORT).show();
+            if (paraderoOrigen.getPosicion() > 0 && paraderoDestino.getPosicion() > 0) {
+                if (rutaCercana(paraderoOrigen, paraderos.get( paraderoAnterior - 1))) {
+                    rutaCercana(paraderos.get( paraderoAnterior - 1), paraderoDestino);
+                    Toast.makeText(getContext(), paraderos.get( paraderoAnterior - 1).getNombre(), Toast.LENGTH_SHORT).show();
                 }
-            }else if(paraderoOrigen.getPosicion()<0 && paraderoDestino.getPosicion()<0){
-                if (rutaCercana(paraderoOrigen, paraderos.get(-1*((int) paraderoAnterior - 1)))) {
-                    rutaCercana(paraderos.get(-1*((int)paraderoAnterior - 1)), paraderoDestino);
+            } else if (paraderoOrigen.getPosicion() < 0 && paraderoDestino.getPosicion() < 0) {
+                if (rutaCercana(paraderoOrigen, paraderos.get(-1 * ( paraderoAnterior - 1)))) {
+                    rutaCercana(paraderos.get(-1 * ( paraderoAnterior - 1)), paraderoDestino);
                 }
             }
+        }catch (ArrayIndexOutOfBoundsException e){
+            if (paraderoOrigen.getPosicion() > 0 && paraderoDestino.getPosicion() > 0) {
+                if (rutaCercana(paraderoOrigen, paraderos.get( paraderoPosterior + 1))) {
+                    rutaCercana(paraderos.get( paraderoPosterior + 1), paraderoDestino);
+                    Toast.makeText(getContext(), paraderos.get( paraderoPosterior + 1).getNombre(), Toast.LENGTH_SHORT).show();
+                }
+            } else if (paraderoOrigen.getPosicion() < 0 && paraderoDestino.getPosicion() < 0) {
+                if (rutaCercana(paraderoOrigen, paraderos.get(-1 * ( paraderoPosterior + 1)))) {
+                    rutaCercana(paraderos.get(-1 * ( paraderoPosterior + 1)), paraderoDestino);
+                }
+            }
+        }
 
 
 
